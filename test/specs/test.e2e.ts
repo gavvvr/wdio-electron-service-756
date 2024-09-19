caf26904e2e4d1346d54c234d54d202cfbf41dcf
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2024-09-19 20:45:10 +0300

    Reproduce issue 756

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -3,6 +3,9 @@ import { browser } from '@wdio/globals'
 describe('Electron Testing', () => {
     it('should print application title', async () => {
         console.log('Hello', await browser.getTitle(), 'application!')
+        await browser.execute(() => {
+            const uploadStub = () => 'boom!'
+        })
     })
 })
 
